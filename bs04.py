7c3e0fe8f36e69e43a2cafd605ab3ee3f3f43778
Favio <faviusam@gmail.com>
2024-05-28 18:12:21 -0500

    Remover carpetas inncesarias

--- bs04.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-try:
-    with open("./Alzheimer_s Dataset/test/NonDemented/26 (65).jpg", "rb") as image_file:
-        base64_image = base64.b64encode(image_file.read()).decode('utf-8')
-        print("Exito!!!")
-        print(base64_image)
-except Exception as e:
-    print(f"Error: {str(e)}")
